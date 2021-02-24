51752279a2a46b69f6b372775bf51fcec9f8af72
nunor <nunomanoje@hotmail.com>
2021-02-24 21:47:00 +0000

    [ Components ] Adding 'Person' component

---
+++ react-learning/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi I'm a react app</h1>
+        <p>we</p>
+        <Person />
+      </div>
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m a react app'))
+  }
+}
+
+export default App;
